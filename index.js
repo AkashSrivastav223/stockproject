fa5944cf75fd27ca237fab88cc1e8accd4dacee6
Akash Srivastav <akash4031@gamil.com>
2021-05-27 14:38:44 +0530

    initial setup for stock app

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//set handlwbars middlware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+var otherstuff = "hello there, this is other stuff ...."
+// set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff: otherstuff
+    });
+});
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+// app.get("/",(req,res)=>{
+// 	res.end("<h1>Hello</h1>");
+// })
+app.listen(PORT,()=>console.log("listening at port "+PORT));
---
+++ readme.txt
@@ -0,0 +1,1 @@
+this the project from the top node course and not the finlatics one
